591708fd56a5bf23600b62527198c0d065e65ba9
Kah Kit Zheng <kahkitzheng@gmail.com>
2022-09-15 22:35:53 +0200

    Add example

---
+++ src/hooks/useHeadingObserver.ts
@@ -0,0 +1,57 @@
+import { useEffect, useState, useRef } from "react";
+
+type TableItem = {
+  id: string | number;
+  text: string;
+  level: number;
+};
+
+const useHeadingObserver = () => {
+  const observer = useRef<any>();
+
+  const [activeId, setActiveId] = useState("");
+  const [headings, setHeadings] = useState([] as TableItem[]);
+
+  // Link each heading with an unique id to scroll to
+  useEffect(() => {
+    Array.from(
+      document.querySelectorAll("h2, h3, h4") as NodeListOf<HTMLHeadingElement>
+    ).map((elem) => (elem.id = elem.innerText));
+  }, []);
+
+  // Create a list of headings to observe to
+  useEffect(() => {
+    const elements = Array.from(
+      document.querySelectorAll("h2, h3, h4") as NodeListOf<HTMLHeadingElement>
+    ).map((elem, index) => ({
+      id: elem.id !== "" ? elem.id : index,
+      text: elem.innerText,
+      level: Number(elem.nodeName.charAt(1)),
+    }));
+
+    setHeadings(elements);
+  }, []);
+
+  // Use the InersectionObserver API to check whether a heading is in view
+  useEffect(() => {
+    const handleObsever = (entries: any) => {
+      entries.forEach((entry: any) => {
+        if (entry?.isIntersecting) setActiveId(entry.target.id);
+      });
+    };
+
+    // Informs the observer which part of the screen should observe.
+    observer.current = new IntersectionObserver(handleObsever, {
+      rootMargin: "0% 0px -50% 0px",
+    });
+
+    const elements = document.querySelectorAll("h2, h3, h4");
+    elements.forEach((elem) => observer.current.observe(elem));
+
+    return () => observer.current?.disconnect();
+  }, []);
+
+  return { activeId, headings };
+};
+
+export default useHeadingObserver;
